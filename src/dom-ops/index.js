077841f377f2aa6a00426b90008ac1b3c8c8f62f
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2017-02-21 20:08:29 +0000

    Move files to src directory

--- src/dom-ops/index.js
+++ src/dom-ops/index.js
@@ -1,26 +1,6 @@
-/**
- * ==============================================================
- * Helpers
- * ==============================================================
- */
-
 export nodesToArray from './helpers/nodes-to-array';
 export whenReady from './helpers/when-ready';
-
-/**
- * ==============================================================
- * Identification
- * ==============================================================
- */
-
 export hasClass from './identification/has-class';
-
-/**
- * ==============================================================
- * Manipulation
- * ==============================================================
- */
-
 export addClass from './manipulation/add-class';
 export appendChild from './manipulation/append-child';
 export clone from './manipulation/clone';
@@ -32,23 +12,9 @@ export insertEnd from './manipulation/insert-end';
 export insertStart from './manipulation/insert-start';
 export removeChild from './manipulation/remove-child';
 export removeClass from './manipulation/remove-class';
-
-/**
- * ==============================================================
- * Selection
- * ==============================================================
- */
-
 export select from './selection/select';
 export selectById from './selection/select-by-id';
 export selectFirst from './selection/select-first';
-
-/**
- * ==============================================================
- * Traversal
- * ==============================================================
- */
-
 export child from './traversal/child';
 export closest from './traversal/closest';
 export nextElement from './traversal/next-element';
